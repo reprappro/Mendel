e2f303ebe8d98830a2b38509280234e1ab54155d
reprappro <reprappro@gmail.com>
2012-02-25 19:56:44 +0000

    Improved design of Y frog so it can be lasered or printed.

---
+++ gEDA/Heatbed/heatbed.cmd
@@ -0,0 +1,50 @@
+# Pin name action command file
+
+# Start of element Q1
+ChangePinName(Q1, 3, S)
+ChangePinName(Q1, 2, D)
+ChangePinName(Q1, 1, G)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CONN6
+ChangePinName(CONN6, 1, 1)
+ChangePinName(CONN6, 2, 2)
+
+# Start of element CONN5
+ChangePinName(CONN5, 1, 1)
+ChangePinName(CONN5, 2, 2)
+
+# Start of element CONN4
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element CONN3
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 4, 4)
+ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 3, 3)
+
+# Start of element CONN1
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
